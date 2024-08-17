0de87e09b6b9f97b596cdecf35c04f6f43783d39
Martín PC <bergesio90@gmail.com>
2024-08-17 20:30:33 +0200

    actualizando scroll infinito

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,13 +6,18 @@ export const Main = () => {
 	const [links, setLinks] = useState([]);
 	const [loading, setLoading] = useState(true);
 
+	useEffect(() => {
+		fetchData('https://dragonball-api.com/api/characters');
+	}, []);
+
 	const fetchData = async (url) => {
 		setLoading(true);
 		const response = await fetch(url);
 		const data = await response.json();
 
-		const newCharacters = data.items;
-		setCharacters((prev) => [...prev, ...newCharacters]);
+		const newCharacters = [...characters, ...data.items];
+		//console.log(newCharacters);
+		setCharacters(newCharacters);
 		console.log(characters);
 
 		const links = data.links;
@@ -22,7 +27,7 @@ export const Main = () => {
 		setLoading(false);
 	};
 
-	const handleScroll = async () => {
+	const handleScroll = () => {
 		const clientHeight = document.documentElement.clientHeight; // Altura de la ventana visible
 		const scrollTop = document.documentElement.scrollTop; // Altura de scroll disponible hacia arriba
 		const scrollHeight = document.documentElement.scrollHeight; // Altura total de todo el contenido
@@ -30,30 +35,29 @@ export const Main = () => {
 		const scrollRemaining = Math.abs(clientHeight + scrollTop - scrollHeight);
 		//console.log(scrollRemaining);
 
-		if (scrollRemaining < 50) {
+		if (scrollRemaining === 0) {
 			//console.log(links.next);
 			fetchData(links.next);
 		}
 	};
 
-	useEffect(() => {
-		fetchData('https://dragonball-api.com/api/characters');
-	}, []);
-
 	document.addEventListener('scroll', handleScroll);
 
 	return (
-		<section className='characters-grid'>
-			{/* {error && <p>{error}</p>} */}
-			{loading && <p>loading...</p>}
-			{characters.map((character) => {
-				return (
-					<Card
-						key={character.id}
-						character={character}
-					/>
-				);
-			})}
-		</section>
+		<>
+			<section className='characters-grid'>
+				{/* {error && <p>{error}</p>} */}
+				{loading && <p>loading...</p>}
+				{characters.map((character) => {
+					return (
+						<Card
+							key={character.id}
+							character={character}
+						/>
+					);
+				})}
+			</section>
+			<p className='more'>&#8659;</p>
+		</>
 	);
 };
